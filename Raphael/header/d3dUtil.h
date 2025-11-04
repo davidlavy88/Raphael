82083bb6359f5a85c541cb208551622fb96ccc8b
David Lavy <david.lavy88@gmail.com>
2025-11-03 23:59:49 -0500

    Move utility functions to d3dUtil file

--- Raphael/header/d3dUtil.h
+++ Raphael/header/d3dUtil.h
@@ -86,3 +86,20 @@ struct MeshGeometry
 		IndexBufferUploader = nullptr;
 	}
 };
+
+class d3dUtil
+{
+public:
+	static Microsoft::WRL::ComPtr<ID3D12Resource> CreateDefaultBuffer(
+		ID3D12Device* device,
+		ID3D12GraphicsCommandList* cmdList,
+		const void* initData,
+		UINT64 byteSize,
+		Microsoft::WRL::ComPtr<ID3D12Resource>& uploadBuffer);
+
+	static Microsoft::WRL::ComPtr<ID3DBlob> CompileShader(
+		const std::wstring& filename,
+		const D3D_SHADER_MACRO* defines,
+		const std::string& entrypoint,
+		const std::string& target);
+};
--- Raphael/source/BoxRenderer.cpp
+++ Raphael/source/BoxRenderer.cpp
@@ -1,10 +1,5 @@
 #include "BoxRenderer.h"
 
-// Forward declaration
-ComPtr<ID3DBlob> CompileShader(const std::wstring& filename, const D3D_SHADER_MACRO* defines, const std::string& entrypoint, const std::string& target);
-ComPtr<ID3D12Resource> CreateDefaultBuffer(ID3D12Device* device, ID3D12GraphicsCommandList* cmdList, const void* initData, UINT64 byteSize, ComPtr<ID3D12Resource>& uploadBuffer);
-
-
 bool BoxRenderer::Initialize(D3D12Device& device, SwapChain& swapChain, HWND hwnd)
 {
     if (!Renderer::Initialize(device, swapChain, hwnd))
@@ -114,8 +109,8 @@ void BoxRenderer::BuildRootSignature(D3D12Device& device)
 void BoxRenderer::BuildShadersAndInputLayout()
 {
     // Implementation for compiling shaders and defining input layout
-    m_vsByteCode = CompileShader(L"Shaders\\color.hlsl", nullptr, "VS", "vs_5_0");
-    m_psByteCode = CompileShader(L"Shaders\\color.hlsl", nullptr, "PS", "ps_5_0");
+    m_vsByteCode = d3dUtil::CompileShader(L"Shaders\\color.hlsl", nullptr, "VS", "vs_5_0");
+    m_psByteCode = d3dUtil::CompileShader(L"Shaders\\color.hlsl", nullptr, "PS", "ps_5_0");
 
     m_inputLayout =
     {
@@ -179,10 +174,10 @@ void BoxRenderer::BuildBoxGeometry(D3D12Device& device)
     D3DCreateBlob(ibByteSize, &mBoxGeo->IndexBufferCPU);
     CopyMemory(mBoxGeo->IndexBufferCPU->GetBufferPointer(), indices.data(), ibByteSize);
 
-    mBoxGeo->VertexBufferGPU = CreateDefaultBuffer(device.GetDevice(),
+    mBoxGeo->VertexBufferGPU = d3dUtil::CreateDefaultBuffer(device.GetDevice(),
         device.GetCommandList(), vertices.data(), vbByteSize, mBoxGeo->VertexBufferUploader);
 
-    mBoxGeo->IndexBufferGPU = CreateDefaultBuffer(device.GetDevice(),
+    mBoxGeo->IndexBufferGPU = d3dUtil::CreateDefaultBuffer(device.GetDevice(),
         device.GetCommandList(), indices.data(), ibByteSize, mBoxGeo->IndexBufferUploader);
 
     mBoxGeo->VertexByteStride = sizeof(Vertex);
@@ -389,88 +384,4 @@ void BoxRenderer::OnMouseMove(WPARAM btnState, int x, int y)
 
     mLastMousePos.x = x;
     mLastMousePos.y = y;
-}
-
-ComPtr<ID3DBlob> CompileShader(
-    const std::wstring& filename,
-    const D3D_SHADER_MACRO* defines,
-    const std::string& entrypoint,
-    const std::string& target)
-{
-    UINT compileFlags = 0;
-#if defined(DEBUG) || defined(_DEBUG)  
-    compileFlags = D3DCOMPILE_DEBUG | D3DCOMPILE_SKIP_OPTIMIZATION;
-#endif
-
-    HRESULT hr = S_OK;
-
-    ComPtr<ID3DBlob> byteCode = nullptr;
-    ComPtr<ID3DBlob> errors;
-    if (FAILED(D3DCompileFromFile(filename.c_str(), defines, D3D_COMPILE_STANDARD_FILE_INCLUDE,
-        entrypoint.c_str(), target.c_str(), compileFlags, 0, &byteCode, &errors)))
-    {
-		throw std::runtime_error("Failed to compile shader");
-    }
-
-    if (errors != nullptr)
-        OutputDebugStringA((char*)errors->GetBufferPointer());
-
-    return byteCode;
-}
-
-ComPtr<ID3D12Resource> CreateDefaultBuffer(
-    ID3D12Device* device,
-    ID3D12GraphicsCommandList* cmdList,
-    const void* initData,
-    UINT64 byteSize,
-    ComPtr<ID3D12Resource>& uploadBuffer)
-{
-    ComPtr<ID3D12Resource> defaultBuffer;
-
-    // Create the actual default buffer resource.
-    CD3DX12_HEAP_PROPERTIES heapPropDefault(D3D12_HEAP_TYPE_DEFAULT);
-    CD3DX12_RESOURCE_DESC resDesc = CD3DX12_RESOURCE_DESC::Buffer(byteSize);
-    device->CreateCommittedResource(
-        &heapPropDefault,
-        D3D12_HEAP_FLAG_NONE,
-        &resDesc,
-        D3D12_RESOURCE_STATE_COMMON,
-        nullptr,
-        IID_PPV_ARGS(defaultBuffer.GetAddressOf()));
-
-    // In order to copy CPU memory data into our default buffer, we need to create
-    // an intermediate upload heap. 
-    CD3DX12_HEAP_PROPERTIES heapPropUpload(D3D12_HEAP_TYPE_UPLOAD);
-    device->CreateCommittedResource(
-        &heapPropUpload,
-        D3D12_HEAP_FLAG_NONE,
-        &resDesc,
-        D3D12_RESOURCE_STATE_GENERIC_READ,
-        nullptr,
-        IID_PPV_ARGS(uploadBuffer.GetAddressOf()));
-
-
-    // Describe the data we want to copy into the default buffer.
-    D3D12_SUBRESOURCE_DATA subResourceData = {};
-    subResourceData.pData = initData;
-    subResourceData.RowPitch = byteSize;
-    subResourceData.SlicePitch = subResourceData.RowPitch;
-
-    // Schedule to copy the data to the default buffer resource.  At a high level, the helper function UpdateSubresources
-    // will copy the CPU memory into the intermediate upload heap.  Then, using ID3D12CommandList::CopySubresourceRegion,
-    // the intermediate upload heap data will be copied to mBuffer.
-    CD3DX12_RESOURCE_BARRIER rbDescDest = CD3DX12_RESOURCE_BARRIER::Transition(defaultBuffer.Get(),
-        D3D12_RESOURCE_STATE_COMMON, D3D12_RESOURCE_STATE_COPY_DEST);
-    CD3DX12_RESOURCE_BARRIER rbDescRead = CD3DX12_RESOURCE_BARRIER::Transition(defaultBuffer.Get(),
-        D3D12_RESOURCE_STATE_COPY_DEST, D3D12_RESOURCE_STATE_GENERIC_READ);
-    cmdList->ResourceBarrier(1, &rbDescDest);
-    UpdateSubresources<1>(cmdList, defaultBuffer.Get(), uploadBuffer.Get(), 0, 0, 1, &subResourceData);
-    cmdList->ResourceBarrier(1, &rbDescRead);
-
-    // Note: uploadBuffer has to be kept alive after the above function calls because
-    // the command list has not been executed yet that performs the actual copy.
-    // The caller can Release the uploadBuffer after it knows the copy has been executed.
-
-
-    return defaultBuffer;
 }
